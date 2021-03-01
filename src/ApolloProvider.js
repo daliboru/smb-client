525b90c958a3dc5a0456a77308a19c92d8b1be97
Dalibor Belic <dalibor@kulturniautomat.com>
2021-03-01 20:45:21 +0100

    edit proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://secure-fjord-24919.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
